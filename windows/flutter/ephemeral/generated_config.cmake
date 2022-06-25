64e607d5b4d072a1ea808964d20ba49918de362c
dheksaaryadityar <dheksaaryadityar@gmail.com>
2022-06-25 18:34:18 +0700

    Project Mobile Programming

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Android\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\xampp\\htdocs\\resep_makanan" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Android\\flutter"
+  "PROJECT_DIR=C:\\xampp\\htdocs\\resep_makanan"
+  "FLUTTER_ROOT=D:\\Android\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\xampp\\htdocs\\resep_makanan\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\xampp\\htdocs\\resep_makanan"
+  "FLUTTER_TARGET=C:\\xampp\\htdocs\\resep_makanan\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\xampp\\htdocs\\resep_makanan\\.dart_tool\\package_config.json"
+)
